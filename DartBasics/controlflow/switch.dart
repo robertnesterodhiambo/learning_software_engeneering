4ee58fa56b1667fcabf6aec8b88fe26018963726
Robert Nester Odhiambo <odhiamborobert447@gmail.com>
2024-07-13 23:19:54 +0300

    switch in flutter

---
+++ DartBasics/controlflow/switch.dart
@@ -0,0 +1,14 @@
+void main() {
+  int child = 1;
+  switch (child) {
+    case 1:
+      print("Bobby Junior is my first kid");
+      break;
+    case 2:
+      print("Princes Junior is my second kid");
+      break;
+    default:
+      print("I only have two kids");
+      break;
+  }
+}
