4c4fef4d3d4aabcfac9eedfc8e4e310c334926a4
Robert Nester Odhiambo <odhiamborobert447@gmail.com>
2024-08-05 23:11:50 +0300

    complex list types

---
+++ DartBasics/4.Handling_Lists_and_Maps/list_complex.dart
@@ -0,0 +1,9 @@
+void main() {
+  Map<String, dynamic> FilmStarwars = {"title": "StarWars", "Year": 1977};
+  Map<String, dynamic> FilmEmpire = {"title": "Empire Strikes", "Year": 1980};
+  Map<String, dynamic> FilmJedi = {"title": "The last Jedi", "Year": 1983};
+  List films = [FilmStarwars, FilmEmpire, FilmJedi];
+  Map<String, dynamic> CurrentFilm = films[0];
+  print(CurrentFilm);
+  print(CurrentFilm["tittle"]);
+}
