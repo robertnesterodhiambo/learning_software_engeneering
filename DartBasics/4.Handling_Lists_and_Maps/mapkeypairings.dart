02bac52556cd15449df02670c8310048d2112ef9
Robert Nester Odhiambo <odhiamborobert447@gmail.com>
2024-09-05 23:05:59 +0300

    Key pairings

--- DartBasics/4.Handling_Lists_and_Maps/mapkeypairings.dart
+++ DartBasics/4.Handling_Lists_and_Maps/mapkeypairings.dart
@@ -1,3 +1,9 @@
 void main() {
-  print("I still rember dart");
+  Map<int, String> mapmonth = {1: "Jan", 2: "Feb", 3: "March"};
+  Map<int, String> moremonth = {4: "Apr", 5: "May", 6: "June"};
+
+  mapmonth.addEntries(moremonth.entries);
+  mapmonth.forEach((key, value) {
+    print("$key, $value");
+  });
 }
