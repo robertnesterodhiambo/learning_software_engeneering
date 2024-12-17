0cc3141ce6cecd5ffde25e4a863d601f83aa47fc
Robert Nester Odhiambo <odhiamborobert447@gmail.com>
2024-12-17 15:17:02 +0300

    revision of variables

---
+++ DartBasics/Revision/vars.dart
@@ -0,0 +1,11 @@
+void main() {
+  int age = 26;
+  double gpa = 4.5;
+  String sname = "Odhiambo";
+  bool conf = true;
+
+  print("I am Robert");
+  print("I am $age years old");
+  print("My second name is $sname");
+  print("It is $conf that I am a man");
+}
