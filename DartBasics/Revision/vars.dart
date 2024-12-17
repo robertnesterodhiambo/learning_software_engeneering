427fb5b398772fea56dbd439cbe52c2adef75a08
Robert Nester Odhiambo <odhiamborobert447@gmail.com>
2024-12-17 15:22:02 +0300

    date today

--- DartBasics/Revision/vars.dart
+++ DartBasics/Revision/vars.dart
@@ -3,9 +3,12 @@ void main() {
   double gpa = 4.5;
   String sname = "Odhiambo";
   bool conf = true;
+  final today = DateTime.now();
 
   print("I am Robert");
   print("I am $age years old");
   print("My second name is $sname");
   print("It is $conf that I am a man");
+  print("My gpa is $gpa");
+  print("The data today is $today");
 }
