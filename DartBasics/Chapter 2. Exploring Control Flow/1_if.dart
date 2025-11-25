8d9b18db65aa56fafa035d81c544907bba32b89a
Robert Nester Odhiambo <odhiamborobert447@gmail.com>
2025-11-25 22:55:01 +0300

    if sattements

---
+++ DartBasics/Chapter 2. Exploring Control Flow/1_if.dart
@@ -0,0 +1,15 @@
+void main(){
+  bool is_footbal = true;
+  if(is_footbal){
+    print("Go Man U");
+  } else {
+    print("Fuck Chelsea");
+  }
+
+  bool is_basket = false;
+  if(is_basket){
+    print("Go LEbrons");
+  } else {
+    print("Curry Sucks");
+  }
+}
