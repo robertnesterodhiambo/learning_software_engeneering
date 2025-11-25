1705ce5081eb2ac01b976ed690d96c164689b63f
Robert Nester Odhiambo <odhiamborobert447@gmail.com>
2025-11-25 22:58:56 +0300

    if:

---
+++ DartBasics/Chapter 2. Exploring Control Flow/2_while.dart
@@ -0,0 +1,16 @@
+void main() {
+  bool is_foot = true;
+  bool is_bask = false;
+
+  if(is_foot){
+    print("Go Man U");
+  } else {
+    print("Chelsea sucks");
+  }
+
+  if(is_bask) {
+    print("Go Lebron");
+  } else {
+    print("Curry Sucks");
+  }
+}
