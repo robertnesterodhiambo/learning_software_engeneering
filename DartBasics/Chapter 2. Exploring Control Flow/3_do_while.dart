3c56ba2f4f533d7f89e782ed532d32d70330be2d
Robert Nester Odhiambo <odhiamborobert447@gmail.com>
2025-11-26 08:49:34 +0300

    do while

---
+++ DartBasics/Chapter 2. Exploring Control Flow/3_do_while.dart
@@ -0,0 +1,14 @@
+void main(){
+  bool is_foot = true;
+  do {
+    print("Manu will never sucks");
+    is_foot = false;
+
+  }while(is_foot);
+
+  bool is_basket = true;
+  do{
+    print("Lebnon is the best");
+    is_basket = false;
+  } while(is_basket);
+}
