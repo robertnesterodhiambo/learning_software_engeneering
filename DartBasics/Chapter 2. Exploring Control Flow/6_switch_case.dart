4f63e86818b634198ba40977a00d28f8fd476b94
Robert Nester Odhiambo <odhiamborobert447@gmail.com>
2025-11-26 12:34:51 +0300

    swithc case

---
+++ DartBasics/Chapter 2. Exploring Control Flow/6_switch_case.dart
@@ -0,0 +1,12 @@
+void main(){
+  int age = 24;
+  switch(age){
+    case 1: print("I am $age years old");
+          break;
+    case 24: print("I am $age years old");
+         break;
+    default: print("Error values");
+        break;
+
+  }
+}
