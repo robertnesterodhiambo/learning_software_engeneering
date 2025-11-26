b709b05cbf027add8d578cfa34637b57f2781a13
Robert Nester Odhiambo <odhiamborobert447@gmail.com>
2025-11-26 11:08:28 +0300

    for loops

---
+++ DartBasics/Chapter 2. Exploring Control Flow/4_for_loop.dart
@@ -0,0 +1,10 @@
+void main(){
+  int max = 10;
+  for(int i = 0; i < max; i++){
+    print("Hello $i number of times");
+  }
+
+  for(int k = 0; k <max; k++){
+    print("We are aleardy $k childresn");
+  }
+}
