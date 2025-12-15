15ecb9248431d2ed446df26b8c731ba52e5fe9f8
Robert Nester Odhiambo <odhiamborobert447@gmail.com>
2025-12-15 20:22:10 +0300

    spaces in folder naming issue

---
+++ DartBasics/Chapter 5. Getting Started with Object-Oriented Dart/1_create_class.dart
@@ -0,0 +1,12 @@
+const NumDays = 7;
+
+class NumDaysLeft {
+  int today = 0;
+  NumDaysLeft(){
+    today =  DateTime.now().weekday.toInt();
+  }
+  int HowManyDaysLeft () {
+    return NumDays - today;
+  }
+}
+// Error 1 My folder has spaces rectifying this 
